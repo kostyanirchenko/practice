d7417aee7b5daa365aa528ac3db8cb9952d4e501
Vitos <Vitos@WIN-S9P9OOKUJPD>
2015-06-23 01:27:25 +0300

    тестируем мы пушем хер зна шо це таке

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -15,6 +15,7 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("trololo");
         // TODO code application logic here
     }
     
