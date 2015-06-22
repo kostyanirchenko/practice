b90d7262df858a675725247459bfdc02ce0d6c31
Студент <@student1>
2015-06-23 02:18:10 +0300

    Доработка задания

---
+++ src/Lab2.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Lab2 Task 2
+ * y = sin(ln|x| - 1)
+ * @author Kostya Nirchenko
+ */
+public class Lab2 {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static double y;
+    private static double x;
+    
+    public static double setY(double x) {
+        y = Math.sin(Math.log(Math.abs(x)) - 1);
+        return y;
+    }   
+       
+    public static void main(String[] args) throws IOException {
+        System.out.print("Введите х ");
+        x = Double.parseDouble(reader.readLine());        
+        System.out.println("Результат расчета: " + setY(x));
+    }
+}
